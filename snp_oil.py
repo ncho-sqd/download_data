499f93d6d312496ef913e4937691e1628eecd6d1
ncho-sqd <cjw.neil@gmail.com>
2016-07-19 02:36:16 -0700

    Added pandas datareader reference

--- snp_oil.py
+++ snp_oil.py
@@ -1,5 +1,13 @@
 from yahoo_finance import Share
 from pprint import pprint
 
+start_dt = '2010-01-01'
+end_dt = '2016-07-01'
+
 snp=Share('^GSPC')
-pprint(snp.get_historical('2010-01-01', '2016-07-01'))
+oil=Share('BZU16.NYM')
+pprint(snp.get_historical(start_dt,end_dt))
+pprint(oil.get_historical(start_dt,end_dt))
+
+http://stackoverflow.com/questions/12433076/download-history-stock-prices-automatically-from-yahoo-finance-in-python
+from pandas.datareader
