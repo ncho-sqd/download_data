38233b8ea3a8cf4ed21a61aa31e79f4a6789ed3d
ncho-sqd <cjw.neil@gmail.com>
2016-07-24 23:28:49 -0700

    Change snp list into data frame and set end date to today

--- snp_oil.py
+++ snp_oil.py
@@ -1,13 +1,18 @@
 from yahoo_finance import Share
 from pprint import pprint
+from tabulate import tabulate
+import pandas as pd
+from datetime import datetime as dt
 
-start_dt = '2010-01-01'
-end_dt = '2016-07-01'
+start_dt = '2016-01-01'
+end_dt = dt.today().strftime("%Y-%m-%d")
 
 snp=Share('^GSPC')
 oil=Share('BZU16.NYM')
-pprint(snp.get_historical(start_dt,end_dt))
-pprint(oil.get_historical(start_dt,end_dt))
 
-http://stackoverflow.com/questions/12433076/download-history-stock-prices-automatically-from-yahoo-finance-in-python
-from pandas.datareader
+snp_list = snp.get_historical(start_dt,end_dt)
+df=pd.DataFrame(snp_list)
+print df
+
+#http://stackoverflow.com/questions/12433076/download-history-stock-prices-automatically-from-yahoo-finance-in-python
+#from pandas.datareader
