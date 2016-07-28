c41c3e8df70d25dfa26b4f2457fd5e73eceb7b46
ncho-sqd <cjw.neil@gmail.com>
2016-07-28 01:53:50 -0700

    Added plotting and created band = max-min data field

--- snp_oil.py
+++ snp_oil.py
@@ -4,8 +4,10 @@ from tabulate import tabulate
 import pandas as pd
 from datetime import datetime as dt
 import statsmodels.formula.api as sm
+import matplotlib.pyplot as plt
+import numpy as np
 
-start_dt = '2015-08-01'
+start_dt = '2011-01-01'
 end_dt = dt.today().strftime("%Y-%m-%d")
 
 snp=Share('^GSPC')
@@ -13,12 +15,20 @@ oil=Share('BZU16.NYM')
 
 snp_list = snp.get_historical(start_dt,end_dt)
 df=pd.DataFrame(snp_list)
+#df["Date"] = dt.strptime(df["Date"], '%Y-%m-%d') #Tried to convert string date into date data type but this didn't work
 df["Adj_Ret"] = df["Adj_Close"].astype(float)/df["Adj_Close"].astype(float).shift(-1)-1
 df["Abs_Adj_Ret"] = abs(df["Adj_Ret"])
 df["Volume"] = df["Volume"].astype(float)
+df["Low"] = df["Low"].astype(float)
+df["High"] = df["High"].astype(float)
+df["Band"] = df["High"] - df["Low"]
 
-result = sm.ols(formula="Abs_Adj_Ret ~ Volume", data=df).fit()
+result = sm.ols(formula="Band ~ Volume", data=df).fit()
 print result.summary()
+
+plt.plot(df["Band"])
+
+plt.show()
 
 
 #http://stackoverflow.com/questions/12433076/download-history-stock-prices-automatically-from-yahoo-finance-in-python
